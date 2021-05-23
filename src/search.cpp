7f89b5abaecf5155dd4a8e859038733175921455
while-TuRe <61506871+while-TuRe@users.noreply.github.com>
2021-05-23 19:09:43 +0800

    Add files via upload
    更新

--- src/search.cpp
+++ src/search.cpp
@@ -1,20 +1,61 @@
 #include"search.h"
+#include"tools.h"
+const bool NO_NULL = false; // "SearchPV()"的参数，是否禁止空着裁剪
 /*******************
 限制宽度的深度优先搜索（迭代深度优先搜索的一步）用alpha-beta算法实现
 根据searchInfo中mvs.mvs中存储的所有走法及顺序，
 beta:本层节点最小值 alpha:上一层节点值
+散列项里到底要保存什么值，并且当你要获取它时怎样来做。
+答案是储存一个值，另加一个标志来说明这个值是什么含义。
 **********************/
-int RootSearch(int depth, int alpha, int beta)
+static int SearchPV(int depth, int alpha, int beta, bool bNoNull = false)
 {
+	int vl,vlBest,nHashFlag;
+	int nNewDepth, nCurrTimer;
+	int mvBest, mvHash, mv;
+	MoveSortStruct MoveSort;
+	// 1. 在叶子结点处调用静态搜索；
 	if (depth <= 0)
 		return searchInfo.board.valueRed - searchInfo.board.valueBlack;
+		//return SearchQuiesc(searchInfo.board, alpha, beta);
 		//return Evaluate(searchInfo.board);
+	
+	// 2. 重复裁剪；
+	//vl = RepPruning(search.board, beta);
+	if (vl > -MATE_VALUE) {//-MATE_VALUE是重复的特定返回值
+		return vl;
+	}
+	// 3. 置换裁剪；如果该搜索节点在置换表中出现
+	vl = probeHash(searchInfo.board, alpha, beta, depth);
+	if (searchInfo.bUseHash && vl > -MATE_VALUE) {
+		// 由于PV结点不适用置换裁剪，所以不会发生PV路线中断的情况
+		return vl;
+	}
+	// 4. 达到极限深度，直接返回评价值；
+	if (searchInfo.board.distance == LIMIT_DEPTH) {
+		return searchInfo.board.Evaluate();
+	}
+	// 5. 尝试空着裁剪；
+	if (bNoNull){ //&& !searchInfo.board.isChecked() && searchInfo.board.nullOkay()) {
+		searchInfo.board.NullMove();
+		vl = -SearchPV(depth - NULL_DEPTH - 1 ,-beta, 1 - beta,NO_NULL);
+		searchInfo.board.UndoNullMove();
+
+		if (vl >= beta) {
+			return vl;
+		}
+	}
+	// 6. 初始化；
+	mvBest = 0;
+	nHashFlag = HASH_ALPHA;
+	vlBest = -MATE_VALUE;
+	MoveSort.Init(mvHash);
 	MoveSortStruct MoveSort;
 	int mv;
 	while ((mv = MoveSort.Next()) != 0) {
 		if (searchInfo.board.makeMove(mv))//不是被将着法，则尝试走
 		{
-			int value = -RootSearch(depth - 1, -beta, -alpha);
+			int value = -SearchPV(depth - 1, -beta, -alpha);
 			searchInfo.board.undoMakeMove();
 			if (value > beta)
 				return beta;
@@ -22,9 +63,54 @@ int RootSearch(int depth, int alpha, int beta)
 				alpha = value;
 		}
 	}
-	searchInfo.alpha = alpha;
-	searchInfo.beta = beta;
+	//searchInfo.alpha = alpha;
+	//searchInfo.beta = beta;
 	return alpha;
+}
+int SearchRoot(int depth) {
+	int nNewDepth, vlBest, vl, mv, nCurrTimer;
+	// 根结点搜索例程包括以下几个步骤：
+	// 1. 初始化
+	vlBest = -MATE_VALUE;
+	searchInfo.mvs.Init(searchInfo.mvResult);
+	// 2. 逐一搜索每个着法
+	while ((mv = searchInfo.mvs.Next()) != 0) {
+		if (searchInfo.board.makeMove(mv)) {
+			// 3. 尝试选择性延伸(只考虑将军延伸)
+			nNewDepth = (searchInfo.board.isChecked() ? depth : depth - 1);
+			// 4. 主要变例搜索
+			if (vlBest == -MATE_VALUE) {
+				vl = -SearchPV(-MATE_VALUE, MATE_VALUE, nNewDepth, NO_NULL);
+			}
+			else {
+				vl = -SearchPV(-vlBest - 1, -vlBest, nNewDepth);
+				if (vl > vlBest) { // 这里不需要" && vl < MATE_VALUE"了
+					vl = -SearchPV(-MATE_VALUE, -vlBest, nNewDepth, NO_NULL);
+				}
+			}
+			searchInfo.board.UndoMakeMove();
+			if (searchInfo.bStop) {
+				return vlBest;
+			}
+			// 5. Alpha-Beta边界判定("vlBest"代替了"SearchPV()"中的"vlAlpha")
+			if (vl > vlBest) {
+				// 6. 如果搜索到第一着法，那么"未改变最佳着法"的计数器加1，否则清零
+				searchInfo.nUnchanged = (vlBest == -MATE_VALUE ? searchInfo.nUnchanged + 1 : 0);
+				vlBest = vl;
+				// 7. 搜索到最佳着法时记录主要变例
+				searchInfo.mvResult = mv;
+			}
+
+			nCurrTimer = (int)(GetTime() - searchInfo.llTime);
+			if (nCurrTimer > searchInfo.nMaxTimer) {
+				searchInfo.bStop = true;
+			}
+
+		}
+	}
+	recordHash(searchInfo.board, HASH_PV, vlBest, depth, searchInfo.mvResult);
+	searchInfo.SetBestMove(searchInfo.mvResult, depth, searchInfo.wmvKiller[searchInfo.board.nDistance]);
+	return vlBest;
 }
 /***********************
 主搜索函数
@@ -32,9 +118,110 @@ int RootSearch(int depth, int alpha, int beta)
 ************************/
 void SearchMain(int depth)
 {
-	for (int i = 0; i < depth; i++)
-	{
-		RootSearch(i,searchInfo.alpha,searchInfo.beta);
+	//searchInfo.ClearHistory();
+	//for (int i = 0; i < depth; i++)
+	//{
+	//	SearchRoot(i);
+
+	//	// 搜索到杀棋则终止搜索
+	//	if (vlLast > WIN_VALUE || vlLast < -WIN_VALUE) {
+	//		break;
+	//	}
+	//}
+	int i, vl, vlLast;
+	int nCurrTimer, nLimitTimer;
+	int nBookMoves;
+	BookStruct bks[MAX_GEN_MOVES];
+	// 主搜索例程包括以下几个步骤：
+
+	// 2. 从开局库中搜索着法
+	//if (Search.bUseBook) {
+	//	// a. 获取开局库中的所有走法
+	//	nBookMoves = GetBookMoves(Search.pos, bks);
+	//	if (nBookMoves > 0) {
+	//		vl = 0;
+	//		for (i = 0; i < nBookMoves; i++) {
+	//			vl += bks[i].wvl;
+	//		}
+	//		// b. 根据权重随机选择一个走法
+	//		vl = Search.rc4Random.NextLong() % (uint32_t)vl;
+	//		for (i = 0; i < nBookMoves; i++) {
+	//			vl -= bks[i].wvl;
+	//			if (vl < 0) {
+	//				break;
+	//			}
+	//		}
+	//		// c. 如果开局库中的着法构成循环局面，那么不走这个着法
+	//		Search.pos.MakeMove(bks[i].wmv);
+	//		if (Search.pos.RepStatus(3) == 0) {
+	//			Search2.mvResult = bks[i].wmv;
+	//			Search.pos.UndoMakeMove();
+	//			uint32_t result = MOVE_COORD(Search2.mvResult);
+	//			printf("bestmove %.4s\n", (const char*)&result);
+	//			fflush(stdout);
+	//			if (Search.bDebug)
+	//				Search.pos.DrawBoard(Search2.mvResult);
+	//			return;
+	//		}
+	//		Search.pos.UndoMakeMove();
+	//	}
+	//}
+
+	// 3. 如果深度为零则返回静态搜索值
+	if (depth == 0) {
+		vl = SearchQuiesc(searchInfo.board, -MATE_VALUE, MATE_VALUE);
+		//		vl = Evaluate(searchInfo.board);
+		if (searchInfo.bDebug) {
+			printf("info depth 0 score %d\n", vl);
+			fflush(stdout);
+		}
+		return;
+	}
+
+	// 4. 初始化时间和计数器
+	searchInfo.bStop = false;
+	searchInfo.nUnchanged = 0;
+	searchInfo.mvResult = 0;
+	searchInfo.ClearKiller(searchInfo.wmvKiller);
+	searchInfo.ClearHistory();
+	memset(searchInfo.HashTable, 0, sizeof(searchInfo.HashTable));
+	// 由于 ClearHash() 需要消耗一定时间，所以计时从这以后开始比较合理
+	searchInfo.llTime = GetTime();
+	vlLast = 0;
+	nCurrTimer = 0;
+
+	// 5. 做迭代加深搜索
+	for (i = 1; i <= depth; i++) {
+		// 6. 搜索根结点
+		vl = SearchRoot(i);
+		if (searchInfo.bStop) {
+			if (vl > -MATE_VALUE) {
+				vlLast = vl; // 跳出后，vlLast会用来判断认输或投降，所以需要给定最近一个值
+			}
+			break; // 没有跳出，则"vl"是可靠值
+		}
+		if (searchInfo.bDebug) {
+			printf("info depth %d score %d\n", i, vl);
+			fflush(stdout);
+		}
+		nCurrTimer = (int)(GetTime() - searchInfo.llTime);
+		// 7. 如果搜索时间超过适当时限，则终止搜索
+		nLimitTimer = searchInfo.nMaxTimer;
+		// a. 如果当前搜索值没有落后前一层很多，那么适当时限减半
+		nLimitTimer = (vl + DROPDOWN_VALUE >= vlLast ? nLimitTimer / 2 : nLimitTimer);
+		// b. 如果最佳着法连续多层没有变化，那么适当时限减半
+		nLimitTimer = (searchInfo.nUnchanged >= UNCHANGED_DEPTH ? nLimitTimer / 2 : nLimitTimer);
+		if (nCurrTimer > nLimitTimer) {
+			vlLast = vl;
+			break; // 不管是否跳出，"vlLast"都已更新
+		}
+
+		vlLast = vl;
+
+		// 8. 搜索到杀棋则终止搜索
+		if (vlLast > WIN_VALUE || vlLast < -WIN_VALUE) {
+			break;
+		}
 
 	}
 	//完成迭代加深搜索，获得结果
--- src/search.h
+++ src/search.h
@@ -5,10 +5,21 @@
 #include<iostream>
 #include"BoardManipulate.h"
 #include"hash.h"
+#include"tools.h"
 //#include"tools.h"
 #include<algorithm>
 const int HASH_SIZE = 1 << 20; // 置换表大小 int64
 const int MAX_GEN_MOVES = 128;
+const int LIMIT_DEPTH = 64;       // 搜索的极限深度
+const int SORT_VALUE_MAX = 65535; // 着法序列最大值
+const int NULL_DEPTH = 2;    // 空着裁剪的深度
+const int IID_DEPTH = 2;         // 内部迭代加深的深度
+const int SMP_DEPTH = 6;         // 并行搜索的深度
+const int UNCHANGED_DEPTH = 4;   // 未改变最佳着法的深度
+
+const int DROPDOWN_VALUE = 20;   // 落后的分值
+const int RESIGN_VALUE = 300;    // 认输的分值
+const int DRAW_OFFER_VALUE = 40; // 提和的分值
 using namespace std;
 
 /***************
@@ -19,20 +30,24 @@ hash置换表结构体
 //	uint32_t dwLock0, dwLock1;	// Zobrist校验锁
 //	uint16_t BestMove;	//最佳着法
 //};
-
+SearchInfo searchInfo;
 extern ZobristTable zobristInfo;
 /**********************
 * 走法存储排序结构体
 ***********************/
-enum State { HashState, AllSearchState, OdinaryState };
+enum State { HashState, PHASE_KILLER_1, PHASE_KILLER_2,AllSearchState, OdinaryState };
 class MoveSortStruct {
 public:
 	uint16_t mvs[MAX_GEN_MOVES];           // 所有的走法
 	int bestmv;	//计算出来的最好走法
 	State state;
-	/*MoveSortStruct(int Bestmv) {
-		bestmv = Bestmv;
-	}*/
+	int mvHash, mvKiller1, mvKiller2; // 置换表走法和两个杀手走法
+	void Init(int mvHash_) { // 初始化，设定置换表走法和两个杀手走法
+		mvHash = mvHash_;
+		mvKiller1 = searchInfo.wmvKiller[searchInfo.board.distance][0];
+		mvKiller2 = searchInfo.wmvKiller[searchInfo.board.distance][1];
+		state = HashState;
+	}
 	int Next();
 };
 /******************
@@ -43,22 +58,43 @@ public:
 	MoveSortStruct mvs;				//根节点着法,目前可以选择的着法
 	HashStruct HashTable[HASH_SIZE];	// 置换表HashTable[x]=搜索得到信息
 	int nHistoryTable[65536];			// 历史表
-	int time;
+	uint16_t wmvKiller[LIMIT_DEPTH][2]; // 杀手着法表
 	int mvResult;			//某一步搜索得到的结果 存储方式：H7-E7	(炮８平５）相当于一个全局变量
 	int alpha, beta;		//beta:本层节点最大值 alpha:上一层节点最大值
+	int vlLast;
 	Board board;                // 待搜索的局面
+	int time;				//限制时间
 	bool bQuit;						   // 是否收到退出指令
 	bool bDebug;					   // 是否调试模式
+	bool bStop;				            // 中止信号
 	bool bUseHash, bUseBook;           // 是否使用置换表裁剪和开局库
 	RC4 rc4Random;               // 随机数
 	int nMaxTimer;					   // 最大使用时间
 	char szBookFile[1024];             // 开局库
+	bool CompareHistory(const int lpmv1, const int lpmv2);
+	void ClearHistory();
+	inline void ClearKiller(uint16_t(*lpwmvKiller)[2]);
+	void SetBestMove(int mv, int nDepth, uint16_t* lpwmvKiller);
 }searchInfo;
 // "sort"按历史表排序的比较函数.排序方式：越好的越靠后
-bool CompareHistory(const int lpmv1, const int lpmv2) {
-	return searchInfo.nHistoryTable[lpmv1] < searchInfo.nHistoryTable[lpmv2];
+bool SearchInfo::CompareHistory(const int lpmv1, const int lpmv2) {
+	return nHistoryTable[lpmv1] < nHistoryTable[lpmv2];
 }
-
+void SearchInfo::ClearHistory() {
+	memset(nHistoryTable, 0, 65536 * sizeof(int));
+}
+// 清空杀手着法表
+inline void SearchInfo::ClearKiller(uint16_t(*lpwmvKiller)[2]) {
+	memset(lpwmvKiller, 0, LIMIT_DEPTH * sizeof(uint16_t[2]));
+}
+// 对最佳走法的处理
+void SearchInfo::SetBestMove(int mv, int nDepth, uint16_t* lpwmvKiller) {
+	nHistoryTable[mv] += nDepth * nDepth;
+	if (lpwmvKiller[0] != mv) {
+		lpwmvKiller[1] = lpwmvKiller[0];
+		lpwmvKiller[0] = mv;
+	}
+}
 
 //bool CompareHistory(const int lpmv1, const int lpmv2);
 int MoveSortStruct::Next() {
@@ -67,10 +103,21 @@ int MoveSortStruct::Next() {
 	case HashState:
 		state = AllSearchState;
 		return bestmv;
+	case PHASE_KILLER_1:
+		state = PHASE_KILLER_2;
+		if (mvKiller1 != mvHash && mvKiller1 != 0 && searchInfo.board.LegalMove(mvKiller1)) {
+			return mvKiller1;
+		}
+		// 2. 杀手着法启发(第二个杀手着法)，完成后立即进入下一阶段；
+	case PHASE_KILLER_2:
+		state = AllSearchState;
+		if (mvKiller2 != mvHash && mvKiller2 != 0 && searchInfo.board.LegalMove(mvKiller2)) {
+			return mvKiller2;
+		}
 	case AllSearchState:
 		state = OdinaryState;
-		index = searchInfo.board.GenMoves(mvs);
-		sort(mvs, mvs + index, CompareHistory);
+		index = searchInfo.board.genMoves(mvs);
+		sort(mvs, mvs + index, SearchInfo::CompareHistory);
 	case OdinaryState:
 		while (index > 0) {
 			int mv = mvs[--index];
@@ -82,6 +129,5 @@ int MoveSortStruct::Next() {
 	}
 
 }
-
 
 #endif
