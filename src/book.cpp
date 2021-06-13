10d2a275a8f481a352c231f4be3b7232c0ce3c56
Feng <64128295+weiii-feng@users.noreply.github.com>
2021-06-13 20:11:32 +0800

    更新ucci及开局库

--- src/book.cpp
+++ src/book.cpp
@@ -1,74 +1,103 @@
 #include "BoardManipulate.h"
 #include "book.h"
+/**********************************
+在eleeye 的 BOOK.DAT提取开局库
+#include<iostream>
+#include <sstream>
+#include <fstream>
+using namespace std;
+struct BookStruct {
+	union {
+		uint32_t dwZobristLock;
+		int nPtr;
+	};
+	uint16_t wmv, wvl;
+};
+
+int main()
+{
+	FILE* fp;
+	int j;
+	BookStruct book;
+	int num = 0;
+	fopen_s(&fp, "BOOK.DAT", "rb");
+	ofstream out("结果.txt", ios::ate);
+	if (fp == NULL)
+		printf("file open error");
+	int pchData[100] = { 0 };
+	uint16_t temp_3[100] = { 0 };
+	while (!feof(fp)) {
+
+		fread(pchData, 4, 1, fp);
+		fread(temp_3, 2, 2, fp);
+		num++;
+		out << "{uint32_t(" << pchData[0] << "),uint16_t(" << temp_3[0] << "),uint16_t(" << temp_3[1] << ")},\n";
+
+	}
+	out << num;
+	out.close();
+	fclose(fp);
+	return 0;
+}
+********************************************/
 
 
 int GetBookMoves(const Board& pos, BookStruct* lpbks) {
-	BookStruct2 book_temp;
-	Board posScan=pos;
 	BookStruct bk;
-	int nScan, nLow, nHigh, nPtr;
+	Board pos_temp=pos;
+	int number,nPtr,l, r;
 	int i, j, move_num;
-	book_temp.init();
-	// 折半查找法搜索；
-	for (nScan = 0; nScan < 2; nScan++) {
-		nPtr = nLow = 0;
-		nHigh = book_temp.nLen - 1;
-		while (nLow <= nHigh) {
-			nPtr = (nLow + nHigh) / 2;
-			book_temp.Read(bk, nPtr);
-			if (BOOK_POS_CMP(bk, posScan) < 0) {
-				nLow = nPtr + 1;
-			}
-			else if (BOOK_POS_CMP(bk, posScan) > 0) {
-				nHigh = nPtr - 1;
-			}
-			else {
+	// 二分查找：
+	for (number = 0; number < 2; number++){
+		nPtr = l = 0;
+		r =nLen - 1;
+		while (l <= r){
+			nPtr = (l + r) / 2;
+			Read(bk, nPtr);
+			if (BOOK_compare(bk, pos_temp) < 0) 
+				l = nPtr + 1;
+			else if (BOOK_compare(bk, pos_temp) > 0) 
+				r = nPtr - 1;
+			else 
 				break;
-			}
 		}
-		if (nLow <= nHigh) {
+		if (l <= r) 
 			break;
-		}
-		// 镜像局面
-		pos.mirror(posScan);
+		// 对称
+		pos.mirror(pos_temp);
 	}
 
-	// 找不到局面；
-	if (nScan == 2) {
+	// 找不到:
+	if (number == 2) 
 		return 0;
-	}
 
-	// 找到局面；
+	// 找到:
 	for (nPtr--; nPtr >= 0; nPtr--) {
-		book_temp.Read(bk, nPtr);
-		if (BOOK_POS_CMP(bk, posScan) < 0) {
+		Read(bk, nPtr);
+		if (!BOOK_compare(bk, pos_temp)) 
 			break;
-		}
 	}
 	move_num = 0;
-	for (nPtr++; nPtr < book_temp.nLen; nPtr++) {
-		book_temp.Read(bk, nPtr);
-		if (BOOK_POS_CMP(bk, posScan) > 0) {
+	for (nPtr++; nPtr < nLen; nPtr++) {
+		Read(bk, nPtr);
+		if (BOOK_compare(bk, pos_temp)) 
 			break;
-		}
-		if (posScan.isLegalMove(bk.wmv)) {
-			// 如果是镜像，做镜像处理：
+
+		// 对称
+		if (pos_temp.isLegalMove(bk.wmv)) {
 			lpbks[move_num].nPtr = nPtr;
-			lpbks[move_num].wmv = (nScan == 0 ? bk.wmv : MOVE_MIRROR(bk.wmv));
+			lpbks[move_num].wmv = (number == 0 ? bk.wmv : mirror_move(bk.wmv));
 			lpbks[move_num].wvl = bk.wvl;
 			move_num++;
-			if (move_num == MAX_GEN_MVS) {
+			if (move_num == MAX_GEN_MVS) 
 				break;
-			}
 		}
 	}
 	//排序
-	for (i = 0; i < move_num - 1; i++) {
-		for (j = move_num - 1; j > i; j--) {
-			if (lpbks[j - 1].wvl < lpbks[j].wvl) {
+	for (i = 0; i < move_num - 1; i++) 
+		for (j = move_num - 1; j > i; j--) 
+			if (lpbks[j - 1].wvl < lpbks[j].wvl) 
 				std::swap(lpbks[j - 1], lpbks[j]);
-			}
-		}
-	}
+
 	return move_num;
 }
--- src/ucci.cpp
+++ src/ucci.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+o#include <cstdio>
 #include <iostream>
 #include <stdio.h>
 #include <windows.h>
@@ -9,37 +9,25 @@
 #include "ucci.h"  
 #pragma comment(lib, "Shlwapi.lib")
 
-/*文件：ucci.cpp
- *
- * 其中第一个解释器"BootLine()"接收引擎启动后的第一行指令
- * 输入"ucci"，返回"Comm_ucci"，其他返回"Comm_none"
- * 前两个解释器等待是否有输入，若没有输入则执行待机指令"Idle()"
- * 第三个解释器"BusyLine()"，只用在引擎思考时,没有输入时直接返回"Comm_none"
- */
-
-const int INPUT_MAX = 8192;
-const int MAX_MOVE_NUM = 1024;
+const int MAX = 1024;
 
 #ifdef _WIN32
 //#include <windows.h>
 inline void Idle(void) {
 	Sleep(1);
 }
+
 #else
 //#include <time.h>
 inline void Idle(void) {
-	timespec tv;
-	tv.tv_sec = 0;
-	tv.tv_nsec = 1000000;
-	nanosleep(&tv, NULL);
 }
 #endif
 
-//收引擎启动后的第一行指令,如果是ucci，则返回Comm_ucci
+
 CommEnum BootLine(void)
 {
-	char Line_Str[INPUT_MAX];
-	while (!std::cin.getline(Line_Str, INPUT_MAX)) {
+	char Line_Str[MAX];
+	while (!std::cin.getline(Line_Str, MAX)) {
 		Idle();
 	}
 	if (strcmp(Line_Str, "ucci") == 0)
@@ -48,23 +36,21 @@ CommEnum BootLine(void)
 		return Comm_none;
 }
 
-//"IdleLine()"是最复杂的UCCI指令解释器，大多数的UCCI指令都由它来解释
+
 CommEnum IdleLine(UCCIComm& UcciComm)
 {
-	//如果bDebug指令为1，输出当前读到的指令
-	char Line_Str[INPUT_MAX];
+	char Line_Str[MAX];
 	char* lp;
-	while (!std::cin.getline(Line_Str, INPUT_MAX)) {
-		//输入一个指令
+	while (!std::cin.getline(Line_Str, MAX)) {
 		Idle();
 	}
+
 	lp = Line_Str;
-	// 1. "isready"指令
 	if (strcmp(lp, "isready") == 0) {
 		return Comm_isready;
 	}
-	// 2. "position {<special_position> | fen <fen_string>} [moves <move_list>]"指令
-	//如果读到position指令，着法列表保存在UcciComm里
+
+	//"position {<special_position> | fen <fen_string>} [move_steps <move_step_list>]"指令,着法列表保存在UcciComm里
 	else if (strncmp(lp, "position ", 9) == 0) {
 		lp += 9;
 		if (strncmp(lp, "startpos ", 8) == 0) {
@@ -82,7 +68,7 @@ CommEnum IdleLine(UCCIComm& UcciComm)
 			//UcciComm.position.pos[i] = '\0';
 
 		}
-		// 然后判断是否指定了FEN串
+		// 指定FEN串
 		else if (strncmp(lp, "fen ", 4) == 0) {
 			lp += 4;
 			UcciComm.position.Fen = lp;
@@ -105,28 +91,27 @@ CommEnum IdleLine(UCCIComm& UcciComm)
 			UcciComm.position.pos [l] = '\0';
 			UcciComm.position.posLen = l;
 		}
-		// 如果两者都不是，就立即返回
+		// 返回
 		else {
 			return Comm_none;
 		}
-		// 然后寻找是否指定了后续着法，即是否有"moves"关键字
-		if (strncmp(lp, "moves ", 6) == 0) {
-			//略过" moves "
+
+		//后续着法
+		if (strncmp(lp, "move_steps ", 6) == 0) {
 			lp += 6;
-			UcciComm.position.Move_Num = min((int)(strlen(lp) + 1) / 5, MAX_MOVE_NUM); 	// "moves"后面的每个着法都是1个空格和4个字符
+			UcciComm.position.move_step_Num = min((int)(strlen(lp) + 1) / 5, MAX); 
 			UcciComm.position.CoordList = lp;
 			return Comm_position;
 		}
 		return Comm_position;
 	}
-	// 3. "go [ponder | draw] <mode>"指令
-	else if (strncmp(lp, "go time ", 8) == 0) 
-	{
+	//go 指令
+	else if (strncmp(lp, "go time ", 8) == 0) {
 		lp += 8;
-		int nRet = 0, nt = 0;
-		if (sscanf_s(lp, "%d", &nRet) > 0) {
-			if (nRet > 0)
-				UcciComm.search.TIME = nRet < 60000000 ? nRet : 60000000;
+		int temp_time = 0, nt = 0;
+		if (sscanf_s(lp, "%d", &temp_time) > 0) {
+			if (temp_time > 0)
+				UcciComm.search.TIME = temp_time < 60000000 ? temp_time : 60000000;
 			else
 				UcciComm.search.TIME = 0;
 		}
@@ -134,29 +119,26 @@ CommEnum IdleLine(UCCIComm& UcciComm)
 			UcciComm.search.TIME = 0;
 		return Comm_go;
 	}
-	// 4. "quit"指令
-	else if (strcmp(lp, "quit") == 0) {
+	// quit指令
+	else if (strcmp(lp, "quit") == 0) 
 		return Comm_quit;
-	}
-	// 5. 无法识别的指令
-	else {
+	//其他
+	else 
 		return Comm_none;
-	}
 }
 
-//第三个解释器只用在引擎思考时，没有输入时直接返回"Comm_none"
 CommEnum BusyLine(UCCIComm& UcciComm)
 {
-	char Line_Str[INPUT_MAX];
+	char Line_Str[MAX];
 	char* lp;
-	if (std::cin.getline(Line_Str, INPUT_MAX)) {
-	// "BusyLine"只能接收"isready"、"quit"这两条指令
-		if (strcmp(Line_Str, "isready") == 0) {
+	if (std::cin.getline(Line_Str, MAX)) {
+
+		if (strcmp(Line_Str, "isready") == 0) 
 			return Comm_isready;
-		}
-		else if (strcmp(Line_Str, "quit") == 0) {
+
+		else if (strcmp(Line_Str, "quit") == 0)
 			return Comm_quit;
-		}
+
 		else
 			return Comm_none;		
 	}
