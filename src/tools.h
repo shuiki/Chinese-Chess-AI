2bd1b5f0d7819ac8ae25f0dbbb315cfb0535c7b4
Nanmor <66045141+NanmorX@users.noreply.github.com>
2021-06-16 16:18:16 +0800

    删除一些没有用到的函数

--- src/tools.h
+++ src/tools.h
@@ -1,20 +1,13 @@
-#ifndef TOOS
-#define TOOS
+#ifndef TOOLS
+#define TOOLS
 #include <iostream>
 #include <sys/timeb.h>
-
 
 inline int64_t GetTime() {
     timeb tb;
     ftime(&tb);
     return (int64_t)tb.time * 1000 + tb.millitm;
 }
-
-const int SimpleValues[48] = {
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  5, 1, 1, 1, 1, 3, 3, 4, 4, 3, 3, 2, 2, 2, 2, 2,
-  5, 1, 1, 1, 1, 3, 3, 4, 4, 3, 3, 2, 2, 2, 2, 2,
-};
 
 inline int PopCnt8(uint8_t uc) {
   int n;
@@ -44,30 +37,6 @@ inline uint32_t BOTH_BITPIECE(int nBitPiece) {
   return nBitPiece + (nBitPiece << 16);
 }
 
-inline int SIDE_TAG(int sd) {
-  return 16 + (sd << 4);
-}
-
-inline int OPP_SIDE_TAG(int sd) {
-  return 32 - (sd << 4);
-}
-
-inline int PIECE_INDEX(int pc) {
-  return pc & 15;
-}
-
-inline int OPP_SIDE(int sd) {
-  return 1 - sd;
-}
-
-inline int SQUARE_FORWARD(int sq, int sd) {
-  return sq - 16 + (sd << 5);
-}
-
-inline int SQUARE_BACKWARD(int sq, int sd) {
-  return sq + 16 - (sd << 5);
-}
-
 inline int RANK_Y(int sq) {
   return sq >> 4;
 }
@@ -80,41 +49,8 @@ inline int SIDE_VALUE(int turn, int value){
     return (turn==1)?value:-value;
 }
 
-inline bool HOME_HALF(int sq, int sd) {
-  return (sq & 0x80) != (sd << 7);
-}
-
-inline bool AWAY_HALF(int sq, int sd) {
-  return (sq & 0x80) == (sd << 7);
-}
-
-inline bool SAME_HALF(int sqSrc, int sqDst) {
-  return ((sqSrc ^ sqDst) & 0x80) == 0;
-}
-
-inline bool DIFF_HALF(int sqSrc, int sqDst) {
-  return ((sqSrc ^ sqDst) & 0x80) != 0;
-}
-
-inline bool WHITE_HALF(int sq){
-  return (sq>>7==1);
-}
-
-inline int RANK_DISP(int y) {
-  return y << 4;
-}
-
-inline int FILE_DISP(int x) {
-  return x;
-}
-
-inline int SIMPLE_VALUE(int pc) {
-  return SimpleValues[pc];
-}
-
 inline uint32_t BIT_PIECE(int pc) {
   return 1 << (pc - 16);
 }
-
 
 #endif
